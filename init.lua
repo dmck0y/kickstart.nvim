31ee29f748b4a8b6a0a1f40ced0cefb574715bc9
David McKoy <davidmckoy@Davids-MacBook-Pro.local>
2023-07-28 17:13:30 -0400

    add more options

--- init.lua
+++ init.lua
@@ -43,7 +43,49 @@ P.S. You can delete this when you're done too. It's your config now :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.wo.relativenumber = true
+vim.opt.autowrite = true
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.completeopt = 'menu,menuone,noselect'
+vim.opt.conceallevel = 3
+vim.opt.confirm = true
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.formatoptions = 'jcroqlnt'
+vim.opt.grepformat = '%f:%l:%c:%m'
+vim.opt.grepprg = 'rg --vimgrep'
+vim.opt.ignorecase = true
+vim.opt.inccommand = 'nosplit'
+vim.opt.laststatus = 0
+vim.opt.list = true
+vim.opt.mouse = 'a'
+vim.opt.number = true
+vim.opt.pumblend = 10
+vim.opt.pumheight = 10
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 4
+vim.opt.sessionoptions = { 'buffers', 'curdir', 'tabpages', 'winsize' }
+vim.opt.shiftround = true
+vim.opt.shiftwidth = 2
+vim.opt.shortmess:append { W = true, I = true, c = true }
+vim.opt.showmode = false
+vim.opt.sidescrolloff = 8
+vim.opt.signcolumn = 'yes'
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.spelllang = { 'en' }
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.tabstop = 2
+vim.opt.termguicolors = true
+vim.opt.timeoutlen = 300
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
+vim.opt.updatetime = 200
+vim.opt.wildmode = 'longest:full,full'
+vim.opt.winminwidth = 5
+vim.opt.wrap = false
+
+vim.g.markdown_recommended_style = 0
 
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
