f7ca1dae01734d5937dded5e545d0bb4d0d3dd5b
David McKoy <davidmckoy@Davids-MacBook-Pro.local>
2023-07-29 14:38:31 -0400

    enable lsp servers

--- init.lua
+++ init.lua
@@ -493,9 +493,9 @@ local servers = {
   -- clangd = {},
   -- gopls = {},
   -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  rust_analyzer = {},
+  tsserver = {},
+  html = { filetypes = { 'html', 'twig', 'hbs' } },
 
   lua_ls = {
     Lua = {
