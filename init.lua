bb6eae7d5d4a4cb59411e866a6dcd18371088628
David McKoy <davidmckoy@Davids-MacBook-Pro.local>
2024-10-04 18:00:39 -0400

    swap command to prefer glance

--- init.lua
+++ init.lua
@@ -134,8 +134,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
       vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
     end
 
-    map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-    map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+    map('gdd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+    map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
     map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
     map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
     map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
--- lua/custom/plugins/glance.lua
+++ lua/custom/plugins/glance.lua
@@ -8,9 +8,9 @@ return {
       top_char = "―",
       bottom_char = "―",
     },
-    vim.keymap.set('n', 'gdd', '<CMD>Glance definitions<CR>'),
-    vim.keymap.set('n', 'grr', '<CMD>Glance references<CR>'),
-    vim.keymap.set('n', 'gyy', '<CMD>Glance type_definitions<CR>'),
-    vim.keymap.set('n', 'gmm', '<CMD>Glance implementations<CR>')
+    vim.keymap.set('n', 'gd', '<CMD>Glance definitions<CR>'),
+    vim.keymap.set('n', 'gr', '<CMD>Glance references<CR>'),
+    vim.keymap.set('n', 'gy', '<CMD>Glance type_definitions<CR>'),
+    vim.keymap.set('n', 'gm', '<CMD>Glance implementations<CR>')
   },
 }
